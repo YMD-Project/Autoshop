f78e255bbaa1df2b096618965434046b7826effa
YMD-Project <yagontsev2018@yandex.ru>
2022-03-10 02:54:21 +0500

    Добавьте файлы проекта.

---
+++ Автомагазин Курсовой/Window_Authorization.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Автомагазин_Курсовой
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            reg = new Window_Registration();
+        }
+        Window_Registration reg;
+
+        private void btnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            reg.Show();
+        }
+    }
+}
